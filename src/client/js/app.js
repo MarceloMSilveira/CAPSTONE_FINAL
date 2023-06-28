607d0dbf7ce4551577f68d44fe00ba681d1ae7f7
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-06-28 16:24:21 -0300

    multiple images working

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -102,12 +102,21 @@ function getImage(place,pixabayResponse) {
 }
 
 function validateWebURL (pixabayResponse,numberOfImg){
-    const randomImgIndex = Math.floor(Math.random() * (numberOfImg) + 1);
-    console.log(`${numberOfImg} hits found, ${randomImgIndex-1} choosed!`)
-    if (!(pixabayResponse.hits[randomImgIndex-1].webformatURL)) {
-        validateWebURL (pixabayResponse,numberOfImg)
-    } else
-        return randomImgIndex-1;
+    try {
+        const randomImgIndex = Math.floor(Math.random() * numberOfImg);
+        console.log(`${numberOfImg} hits found, ${randomImgIndex} chosen!`);
+    
+        if (pixabayResponse.hits[randomImgIndex].webformatURL === undefined) {
+          throw new Error('Variável indefinida');
+        }
+    
+        return randomImgIndex;
+      } catch (error) {
+        // Trate a exceção aqui, por exemplo, exiba uma mensagem de erro ou execute alguma ação alternativa
+        console.log('Ocorreu um erro:', error.message);
+        // Você pode chamar a função novamente para tentar encontrar um valor válido
+        return validateWebURL(pixabayResponse, numberOfImg);
+      }
 }
 
 function unknowCity(cityUTF8){
