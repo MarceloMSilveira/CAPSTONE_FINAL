b0f565cc830d211ac7854ad635f95173fd4c56f1
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-06-28 15:15:45 -0300

    safe point

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -7,9 +7,13 @@ const goWeatherBit = async(data) => {
     // how many days is the trip from now (calculating)
     const currentDate = new Date();
     const tripDate = new Date(document.getElementById('tripDate').value);
+    const tripBack = new Date(document.getElementById('backDate').value)
     const diffTime = Math.abs(tripDate - currentDate);
+    const diffTimeOfTrip = Math.abs(tripBack - tripDate);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    const diffDaysOfTrip = Math.ceil(diffTimeOfTrip / (1000 * 60 * 60 * 24))+1
     document.getElementById('when').innerHTML=`Your travel is ${diffDays} days from now!`;
+    document.getElementById('howLong').innerHTML=`And it will be ${diffDaysOfTrip} days long.`;
 
     //decide if travel will be next week
 
@@ -25,7 +29,7 @@ function currentWeather (lat, lon) {
     fetch (weatherBitURL)
     .then(res => res.json())
     .then(resp => {
-        const msg = `The current weather in ${resp.data[0].city_name} is ${resp.data[0].weather.description} Temperature: ${resp.data[0].temp}`;
+        const msg = `The current weather in ${resp.data[0].city_name} is ${resp.data[0].weather.description} Temperature: ${resp.data[0].temp}ºC`;
         document.getElementById('result').innerHTML = msg;
     })
 }
@@ -36,7 +40,7 @@ function nextWeekForecast (lat, lon) {
     fetch (weatherBitURL)
     .then(res => res.json())
     .then(resp => {
-        const msg = `The forecast (next week) weather in ${resp.city_name} is ${resp.data[6].weather.description}, <span>HIGH:</span>${resp.data[6].high_temp} <span>LOW:</span>${resp.data[6].low_temp}`;
+        const msg = `The forecast (next week) weather in ${resp.city_name} is ${resp.data[6].weather.description}, <span>HIGH: </span>${resp.data[6].high_temp} ºC <span>LOW: </span>${resp.data[6].low_temp} ºC`;
         document.getElementById('result').innerHTML = msg;
 
     })
@@ -47,28 +51,63 @@ const getGeonamesData = async (city) =>
     const geonamesURL = `http://api.geonames.org/searchJSON?q=${city}&maxRows=1&username=marcelomsilveira`
     fetch(geonamesURL)
     .then(response => response.json())
-    .then(data => goWeatherBit(data))
+    .then(data => {
+        //data.geonames[0].countryName
+        goWeatherBit(data)})
     .catch(error => console.log(error));
 }
 
-const getPixabayData = async (userCity,city) =>
+const getPixabayData = async (userCity,cityUTF8) =>
 {
-    const pixabayURL = `https://pixabay.com/api/?key=36921349-37b715f1fda946c5428d9d405&q=${city}+tourism&image_type=photo&category=travel`
+    const pixabayURL = `https://pixabay.com/api/?key=36921349-37b715f1fda946c5428d9d405&q=${cityUTF8}+tourism&image_type=photo&category=travel`
     fetch(pixabayURL)
     .then(response => response.json())
-    .then(data => getImage(userCity,data))
+    .then(
+        data => { 
+            if (data.total>0)
+                getImage(userCity,data)
+            else   
+                unknowCity(cityUTF8)
+        }
+    )
     .catch(error => console.log(error));
 }
 
-function getImage(userCity,pixabayResponse) {
+function getCountryImage (countryName) {
+    const pixabayURL = `https://pixabay.com/api/?key=36921349-37b715f1fda946c5428d9d405&q=${countryName}+tourism&image_type=photo&category=travel`
+    fetch(pixabayURL)
+    .then(response => response.json())
+    .then(
+        data => { 
+            getImage(countryName,data)    
+        }
+    )
+    .catch(error => console.log(error))
+}
+
+function getImage(place,pixabayResponse) {
     // Img element acess
     const imgElement = document.getElementById('imgFromPixabay');
     
     // URL of image link to pixabayResp
-    const imgURL = pixabayResponse.hits[0].webformatURL;
-   
+    const numberOfImg = pixabayResponse.total;
+    const randomImgIndex = Math.floor(Math.random() * (numberOfImg) + 1);
+    console.log(`Nr of img of ${place}: ${numberOfImg}. Index of choosed img: ${randomImgIndex-1}`)
+    const imgURL = pixabayResponse.hits[randomImg-1].webformatURL;
     imgElement.style.backgroundImage = `url(${imgURL})`;
-    imgElement.ariaLabel = `image of ${userCity}`
+    imgElement.ariaLabel = `image of ${place}`
+    document.getElementById('myTrip').innerHTML = `Take a look at ${place}:`
+}
+
+function unknowCity(cityUTF8){
+    const geonamesURL = `http://api.geonames.org/searchJSON?q=${cityUTF8}&maxRows=1&username=marcelomsilveira`
+    fetch(geonamesURL)
+    .then(response => response.json())
+    .then(data => {
+        const countryName = data.geonames[0].countryName;
+        getCountryImage(countryName)        
+    })
+    .catch(error => console.log(error));
 }
 
 const postData = async ( url = '', data = {})=>{
