7112b70dde1f299a2645732d7b4790925708296b
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-06-29 14:18:45 -0300

    create jest test file

---
+++ __test__/checkWebURLImg.test.js
@@ -0,0 +1,28 @@
+// Importe a função que será testada
+import { validateWebURL } from "../src/client";
+
+// Crie um mock para o objeto pixabayResponse
+const pixabayResponse = {
+  hits: [
+    { webformatURL: 'https://example.com/image1.jpg' },
+    { webformatURL: 'https://example.com/image2.jpg' },
+    { webformatURL: undefined },
+  ],
+};
+
+describe('validateWebURL', () => {
+  it('retorna um índice válido', () => {
+    const numberOfImg = pixabayResponse.hits.length;
+    const randomImgIndex = validateWebURL(pixabayResponse, numberOfImg);
+
+    expect(randomImgIndex).toBeGreaterThanOrEqual(0);
+    expect(randomImgIndex).toBeLessThan(numberOfImg);
+  });
+
+  it('lança uma exceção quando a webformatURL é indefinida', () => {
+    const numberOfImg = pixabayResponse.hits.length - 1;
+    expect(() => {
+      validateWebURL(pixabayResponse, numberOfImg);
+    }).toThrow('Variável indefinida');
+  });
+});
