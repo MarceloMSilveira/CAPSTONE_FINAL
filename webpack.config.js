081c20330a65308eefac9a0e01c72206f4e44a23
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2023-07-02 08:12:28 -0300

    add Client library in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,13 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'production',
     module: {
-        rules: [
+       rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
